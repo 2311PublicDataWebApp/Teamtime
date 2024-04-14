872b0241c115dd567a8287668bf9e612e00df683
주 <152952334+dpfxm@users.noreply.github.com>
2024-04-14 15:02:36 +0900

    [ADD] 24/04/14 Spring Security 정적 파일 ignore 설정 및 리소스 파일 추가
    1. Spring Security 정적 자원 ignore 설정
     - WebSecurityCustomizer과 requestMatchers() 통해 사용 가능, Security Config 참조
    2. CSS, JS 및 이미지 파일 추가
     - 부트스트랩 CSS, JS 추가
     - 로고 이미지 추가
    3. 회원 기능 html에 css 링크 추가

--- src/main/java/com/teamtime/tt/user/config/SecurityConfig.java
+++ src/main/java/com/teamtime/tt/user/config/SecurityConfig.java
@@ -1,29 +1,39 @@
 package com.teamtime.tt.user.config;
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig {
+public class SecurityConfig{
 
 	@Bean
 	public BCryptPasswordEncoder bCryptPasswordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
-
+	
+	@Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring()
+                .requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+    }
+	
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		http.authorizeHttpRequests((auth) -> auth
 				.requestMatchers("/admin/**").hasRole("ADMIN")
-//				.requestMatchers("/user/**").hasRole("USER")
-				.requestMatchers("/", "/user/**").permitAll()
+				.requestMatchers("/css/**").permitAll()
+                .requestMatchers("/js/**").permitAll()
+                .requestMatchers("/imges/**").permitAll()
+                .requestMatchers("/user/**").permitAll()
+				.requestMatchers("/").permitAll()
 				.anyRequest().authenticated()
-//				.anyRequest().permitAll()
 		);
 
 		http.formLogin((auth) -> auth
@@ -47,7 +57,7 @@ public class SecurityConfig {
 		http.csrf((auth) -> auth
 				.disable()
 		);
-
+		
 		return http.build();
 	}
 	
