128237567cd1c9dc12d8f910e53dcc21af6e1bee
nawhgnues13 <nawhgnues1313@gmail.com>
2024-04-24 14:46:56 +0900

    [ADD] 24/04/24 팀 등록 기능 추가
    1. 팀 등록 기능 추가 중

--- src/main/java/com/teamtime/tt/team/model/dto/Team.java
+++ src/main/java/com/teamtime/tt/team/model/dto/Team.java
@@ -1,5 +1,24 @@
 package com.teamtime.tt.team.model.dto;
 
-public class Team {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@RequiredArgsConstructor
+@AllArgsConstructor
+public class Team {
+	
+	@NonNull
+	private Integer teamNo;
+	private String userId;
+	private Integer chatroomNo;
+	private String teamName;
 }
