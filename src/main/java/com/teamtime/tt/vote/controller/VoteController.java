61a112de0231a03a4ac77a87a759d5a9de38a8f8
nawhgnues13 <nawhgnues1313@gmail.com>
2024-04-26 17:40:05 +0900

    [ADD] 24/04/26 투표 기능 수정
    1. 투표 목록 추가 중
    
    2. 투표 등록 기능 수정

--- src/main/java/com/teamtime/tt/vote/controller/VoteController.java
+++ src/main/java/com/teamtime/tt/vote/controller/VoteController.java
@@ -1,5 +1,7 @@
 package com.teamtime.tt.vote.controller;
 
+import java.util.List;
+
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
@@ -8,9 +10,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.teamtime.tt.alarm.model.dto.Alarm;
+import com.teamtime.tt.alarm.model.service.AlarmService;
+import com.teamtime.tt.team.model.dto.Team;
+import com.teamtime.tt.team.model.service.TeamService;
+import com.teamtime.tt.user.model.dto.User;
+import com.teamtime.tt.user.model.service.UserService;
 import com.teamtime.tt.vote.model.dto.Vote;
 import com.teamtime.tt.vote.model.service.VoteService;
 
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 
 @Controller
@@ -19,9 +28,35 @@ import lombok.RequiredArgsConstructor;
 public class VoteController {
 	
 	private final VoteService vService;
+	private final UserService uService;
+	private final AlarmService aService;
+	private final TeamService tService;
+	
+	@GetMapping("/list.do")
+	public String showVoteList(@AuthenticationPrincipal UserDetails userDetails
+			, HttpSession session
+			, Model model) {
+		String userId = userDetails.getUsername();
+		User user = uService.selectUserById(userId);
+		List<Team> tList = tService.selectTeamById(userId);
+		List<Alarm> aList = aService.selectUnreadAlarm(userId);
+		model.addAttribute("user", user);
+		session.setAttribute("aList", aList);
+		session.setAttribute("tList", tList);
+		return "/vote/voteList";
+	}
 	
 	@GetMapping("/insert.do")
-	public String showInsertVote() {
+	public String showInsertVote(@AuthenticationPrincipal UserDetails userDetails
+			, HttpSession session
+			, Model model) {
+		String userId = userDetails.getUsername();
+		User user = uService.selectUserById(userId);
+		List<Team> tList = tService.selectTeamById(userId);
+		List<Alarm> aList = aService.selectUnreadAlarm(userId);
+		model.addAttribute("user", user);
+		session.setAttribute("aList", aList);
+		session.setAttribute("tList", tList);
 		return "/vote/insertVote";
 	}
 	
